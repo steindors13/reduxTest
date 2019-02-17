e0a75cacc0eed1caacd7d58aa1a9510bf19688ef
Steindor Snorrason <steindorsnorrason@Steindors-MacBook-Pro.local>
2019-02-17 13:56:31 +0000

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './components/store'
+import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
-        <header className="App-header">
+        <header className="App">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Postform />
+          <hr />
+          <Posts />
         </header>
       </div>
+      </Provider>
     );
   }
 }
